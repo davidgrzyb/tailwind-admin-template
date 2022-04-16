e27c9705d0b19f0877a2b9e3e6699a88c59e3252
Cesar Estrada <cesar@codigomovil.mx>
2022-04-16 18:37:27 -0500

    Update to TW3 with node dependancies

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: "#3d68ff",
+          medium: "#1947ee",
+          dark: "#0038fd",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
